e9a5526f72f226b0e67b2cae345513a2812e58db
Raleigh Matlock <raleighman2@gmail.com>
2016-05-27 14:37:45 +0200

    BatteryService: Fully charged notification toggle (1/2)
    Change-Id: I269606e054899e822d98e72db75e4ac3bacddc43
    Signed-off-by: Zdrowy Gosciu <ZdrowyGosciu+GITHUB@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3866,6 +3866,12 @@ public final class Settings {
         public static final String HEADS_UP_SNOOZE_LENGTH_MS = "heads_up_snooze_length_ms";
 	
 	/**
+         * Whether to show the battery fully charged notification
+         * @hide
+         */
+        public static final String BATTERY_FULLY_CHARGED_NOTIFICATION = "battery_fully_charged_notification";
+
+	/**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
