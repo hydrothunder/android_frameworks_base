622465a147891e7a1c8e806590c11ad33ceba883
Joshndroid <fmxjoshiewah@gmail.com>
2016-07-24 20:28:39 +1000

    Revert "Option to show four tiles per row"
    This reverts commit bdc9a924563210348d7351ffbff5c9097fe7e588.

--- packages/SystemUI/src/com/android/systemui/qs/QSDragPanel.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSDragPanel.java
@@ -18,7 +18,6 @@ package com.android.systemui.qs;
 
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
-import android.animation.ObjectAnimator;
 import android.app.ActivityManager;
 import android.content.ContentResolver;
 import android.content.Context;
@@ -35,7 +34,6 @@ import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Handler;
 import android.os.UserHandle;
-import android.provider.Settings;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.util.ArrayMap;
@@ -117,7 +115,6 @@ public class QSDragPanel extends QSPanel implements View.OnDragListener, View.On
     private int mLocationHits;
     private int mLastLeftShift = -1;
     private int mLastRightShift = -1;
-    private int mNumberOfColumns;
     private boolean mRestored;
     private boolean mRestoring;
     // whether the current view we are dragging in has shifted tiles
@@ -538,22 +535,6 @@ public class QSDragPanel extends QSPanel implements View.OnDragListener, View.On
 
     public boolean isEditing() {
         return mEditing;
-    }
-
-    /**
-     * Use three or four columns.
-     */
-    private int useFourColumns() {
-        final Resources res = mContext.getResources();
-        boolean shouldUseFourColumns = Settings.System.getInt(
-            mContext.getContentResolver(), Settings.System.QS_USE_FOUR_COLUMNS,
-                0) == 1;
-        if (shouldUseFourColumns) {
-            mNumberOfColumns = 4;
-        } else {
-            mNumberOfColumns = res.getInteger(R.integer.quick_settings_num_columns);
-        }
-        return mNumberOfColumns;
     }
 
     protected int getPagesForCount(int tileCount) {
@@ -1912,7 +1893,7 @@ public class QSDragPanel extends QSPanel implements View.OnDragListener, View.On
 
     public void updateResources() {
         final Resources res = mContext.getResources();
-        final int columns = Math.max(1, useFourColumns());
+        final int columns = Math.max(1, res.getInteger(R.integer.quick_settings_num_columns));
         mCellHeight = res.getDimensionPixelSize(R.dimen.qs_tile_height);
         mCellWidth = (int) (mCellHeight * TILE_ASPECT);
         mLargeCellHeight = res.getDimensionPixelSize(R.dimen.qs_dual_tile_height);
